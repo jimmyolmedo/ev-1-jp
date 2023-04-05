e1e762eb882a266025a12a8477c57c4bf9b08f98
Hecthor1975 <leivahector009@gmail.com>
2023-04-05 16:29:05 -0400

    qnim

--- Assets/Scripts/Reparar.cs
+++ Assets/Scripts/Reparar.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Reparar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int reparacion = 50;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D elOtroCollision)
     {
-        
+        if(elOtroCollision.CompareTag("Player"))
+        {
+            elOtroCollision.gameObject.GetComponent<Player>().hacha.hpHacha.valor += reparacion;
+
+            Destroy(gameObject);
+        }
     }
 }
